f16a95632764014df675258fc90a6a1618694b31
Pratish Rupesh Dalal <prd1999@hotmail.com>
2020-03-28 10:55:22 -0400

    pixel changes color, 3 switches (bpp) for now

--- EtchASketch.c
+++ EtchASketch.c
@@ -7,6 +7,7 @@ volatile int pixel_buffer_start; // global variable
 void plot_pixel(int x, int y, short int line_color);
 void clear_screen();
 void wait_for_vsync();
+short int pixel_color (int r, int g, int b);
 
 int main(void)
 {
@@ -19,14 +20,14 @@ int main(void)
     //declare your other variables here
     int up_down_keys = 0, left_rt_keys = 0, pixel_inc = 1;
     int x_pos = 150, y_pos = 120;
-    short int colour = 0xFFFF;
+	int SW0 = 0, SW1 = 0, SW2 = 0, SW3 = 0, SW4 = 0, SW5 = 0, SW6 = 0, SW7 = 0, SW8 = 0, SW9 = 0; 
+	volatile int* switch_data_reg = (int*)0xFF200040;
 
     /* set front pixel buffer to a different address than back buffer */
     *(pixel_ctrl_ptr + 1) = 0xC8000000; // first store the address in the back buffer
     wait_for_vsync(); //swap buffers
     pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
     clear_screen();
-
     while (1)
     {
         //poll keys. Increment pixel position
@@ -59,21 +60,36 @@ int main(void)
 
         //poll switches
 
-        //...
+        int switch_data = *switch_data_reg;
+		SW0 = switch_data & 0b0000000001;
+		SW1 = switch_data & 0b0000000010;
+		SW2 = switch_data & 0b0000000100;
+		SW3 = switch_data & 0b0000001000;
+		SW4 = switch_data & 0b0000010000;
+		SW5 = switch_data & 0b0000100000;
+		SW6 = switch_data & 0b0001000000;
+		SW7 = switch_data & 0b0010000000;
+		SW8 = switch_data & 0b0100000000;
+		SW9 = switch_data & 0b1000000000;
 
         //change colour based on switches 8-0
 
-        //...
+        short int color = pixel_color (SW0, SW1, SW2);
 
         //if switch 9 ON, clear screen
 
-        //...
+		if(SW9)
+			clear_screen();
 
         //draw the pixel
-        plot_pixel(x_pos, y_pos, colour);
+        plot_pixel(x_pos, y_pos, color);
 
         wait_for_vsync(); // swap front and back buffers on VGA vertical sync
     }
+}
+
+short int pixel_color (int r, int g, int b) { //3 bit color setup......just for initial testing
+    return ((r<<10) + (g<<4) + b);
 }
 
 void plot_pixel(int x, int y, short int line_color)
